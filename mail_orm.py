b5b69d6a1eb093b850e4e7566816ea4b65e0b6ec
Lydia Rodriguez <Lydia Rodriguez>
2016-06-13 14:12:21 -0500

    Updated mail_orm and pm_hook to add printing with client table

--- mail_orm.py
+++ mail_orm.py
@@ -13,3 +13,13 @@ class Message(Base):
     message_read = Column(Boolean, default=False)
     time_received = Column(Float, default=0)
     ttl_ts = Column(Float, default=0)
+
+class Client(Base):
+    __tablename__ = 'clients'
+    client_id = Column(Integer, primary_key=True)
+    client_name = Column(Text)
+    client_email = Column(Text)
+    synonym1 = Column(Text)
+    synonym2 = Column(Text)
+    synonym3 = Column(Text)
+
--- pm_hook.py
+++ pm_hook.py
@@ -1,4 +1,7 @@
+from mail_orm import Client
+
 def process_message_hook(peer, mailfrom, rcpttos, data):
     print("Message processed.")
     print(mailfrom)
-    
+    if (Client.client_name in mailfrom):
+        print("confirmed. email belongs to client: " + mailfrom)
