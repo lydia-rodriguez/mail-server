680a6dbc3e2c95455158c7e959796997b9814936
Lydia Rodriguez <Lydia Rodriguez>
2016-06-13 14:24:54 -0500

    Updated pm_hook to debug. added for statement

--- pm_hook.py
+++ pm_hook.py
@@ -3,5 +3,6 @@ from mail_orm import Client
 def process_message_hook(peer, mailfrom, rcpttos, data):
     print("Message processed.")
     print(mailfrom)
-    if (Client.client_name in mailfrom):
-        print("confirmed. email belongs to client")
+    for email in Client.client_email:
+        if (email in mailfrom):
+            print("confirmed. email belongs to client")
