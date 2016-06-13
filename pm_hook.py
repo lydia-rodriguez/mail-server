6f30156e9d221185e4633220bf71086fd74210d9
Lydia Rodriguez <Lydia Rodriguez>
2016-06-13 16:27:51 -0500

    modified pm_hook, with help. added select.

--- pm_hook.py
+++ pm_hook.py
@@ -1,11 +1,14 @@
 from mail_orm import Client
+from sqlalchemy import select
 
 
 def process_message_hook(peer, mailfrom, rcpttos, data):
     print("Message processed.")
     print(mailfrom)
-    for c in Client.__table__.columns:
-        print(c)
 
-    for c in Client.__table__.foreign_keys:
-        print(c)
+    clientsList = select([Client.c.client_email]).where(
+        Client.c.client_email.in_(mailfrom)
+    )
+
+    for x in clientsList:
+        print(x)
