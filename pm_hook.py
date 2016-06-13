ea46a01921fd76c7070c3af590618746079dd0fd
Lydia Rodriguez <Lydia Rodriguez>
2016-06-13 16:37:44 -0500

    changed pm_hook to use .having

--- pm_hook.py
+++ pm_hook.py
@@ -6,9 +6,5 @@ def process_message_hook(peer, mailfrom, rcpttos, data):
     print("Message processed.")
     print(mailfrom)
 
-    clientsList = select([Client.c.client_email]).where(
-        Client.c.client_email.in_(mailfrom)
-    )
-
-    for x in clientsList:
-        print(x)
+    clients_list = select([Client.client_email]).having(mailfrom)
+    print(clients_list)
