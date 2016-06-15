447f09d08db2ad4bea830faae34418ba4760705a
Lydia Rodriguez <Lydia Rodriguez>
2016-06-15 16:49:18 -0500

    updated select again

--- pm_hook.py
+++ pm_hook.py
@@ -6,11 +6,8 @@ def process_message_hook(peer, mailfrom, rcpttos, data, engine):
     print("Message processed.")
     print(mailfrom)
 
-    clients_list = select([Client.client_email]).having(mailfrom)
-    print(clients_list)
-
     client_email = column('client_email')
-    s = select([Client.c.client_email]).where(Client.c.client_email == (str(mailfrom)))
+    s = select([Client.client_email]).where(Client.client_email == (str(mailfrom)))
 
     results = engine.execute(s)
 
