487bc44e44c872479f0ed61df534b5625bbd98f3
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2016-06-20 12:13:00 -0500

    modified conditional some more, again

--- pm_hook.py
+++ pm_hook.py
@@ -4,16 +4,17 @@ from sqlalchemy import select, column
 
 def process_message_hook(peer, mailfrom, rcpttos, data, engine):
     print("\nMessage processed.")
-    print(mailfrom)
+    # print(mailfrom)
 
     s = select([Client.client_name]).where(Client.client_email == (str(mailfrom)))
     results = engine.execute(s)
+
     for row in results:
         print("\n".join(row))
 
-        if s is None:
+        if not row:
             print("No match found. Please resend email.")
-        if s is not None:
+        if row:
             print("Match(es) found:")
             print("\n".join(row))
         else:
