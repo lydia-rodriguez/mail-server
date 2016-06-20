57802d3613ab7655985b608e7f6c354416dba30b
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2016-06-20 12:59:28 -0500

    reverted

--- pm_hook.py
+++ pm_hook.py
@@ -4,23 +4,9 @@ from sqlalchemy import select, column
 
 def process_message_hook(peer, mailfrom, rcpttos, data, engine):
     print("\nMessage processed.")
-    # print(mailfrom)
 
     s = select([Client.client_name]).where(Client.client_email == (str(mailfrom)))
     results = engine.execute(s)
-<<<<<<< HEAD
 
     for row in results:
-        # print("\n".join(row))
-        # print(row)
-        if 'FSG_TEST' in row:
-            print("Found")
-        else:
-            print("Error. Please debug.")
-=======
-    if s is not None:
-        for row in results:
-            print("Match found: ".join(row))
-    else:
-        print("No match found. Please resend email.")
->>>>>>> parent of 3976c1e... added print statements to test added conditional statements, updated
+        print("\n".join(row))
