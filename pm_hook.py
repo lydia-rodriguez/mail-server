193477fbb6508e166c6107189ce9a5e1cbe05104
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2016-06-20 11:30:09 -0500

    modified select statement

--- pm_hook.py
+++ pm_hook.py
@@ -6,7 +6,7 @@ def process_message_hook(peer, mailfrom, rcpttos, data, engine):
     print("\nMessage processed.")
     print(mailfrom)
 
-    s = select([Client.client_email]).where(Client.client_email == (str(mailfrom)))
+    s = select([Client.client_name]).where(Client.client_email == (str(mailfrom)))
     results = engine.execute(s)
     for row in results:
         print("\n".join(row))
