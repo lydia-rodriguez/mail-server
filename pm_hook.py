fb4f880c1212577f3998ecbf0f846fd0ce9d7d52
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2016-06-20 12:41:25 -0500

    moved conditional around, 9.4

--- pm_hook.py
+++ pm_hook.py
@@ -9,12 +9,8 @@ def process_message_hook(peer, mailfrom, rcpttos, data, engine):
     s = select([Client.client_name]).where(Client.client_email == (str(mailfrom)))
     results = engine.execute(s)
 
-
     for row in results:
-        c = str(row)
-        print(c)
-
-        if 'FSG' in c:
+        if 'FSG' in str(row):
             print("Found>")
         else:
             print("Error. Please debug.")
@@ -27,8 +23,8 @@ def process_message_hook(peer, mailfrom, rcpttos, data, engine):
 
 
             #     # print(row)
-    #     if 'FSG_TEST' in results:
-    #         print("Match(es) found:")
-    #         print("\n".join(row))
-    #     else:
-    #         print("Error. Please debug.")
+            #     if 'FSG_TEST' in results:
+            #         print("Match(es) found:")
+            #         print("\n".join(row))
+            #     else:
+            #         print("Error. Please debug.")
