2c7562021c87fef4c2784e1841c9a781e3a33c68
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2016-06-22 14:17:51 -0500

    adding sites table, 1.6

--- pm_hook.py
+++ pm_hook.py
@@ -6,19 +6,29 @@ from mail_orm import Client, Site
 def process_message_hook(self, peer, mailfrom, rcpttos, data, engine):
     print("\nMessage processed.")
 
-    client = ''
+    client_id = ''
 
     # Use this query to test if mailfrom value can be found in client_email column of clients table
-    # client_email_mailfrom = select([Client.client_name]).where(Client.client_email == (str(mailfrom)))
-    client_email_mailfrom = select([Client.client_id]).where(Client.client_email == (str(mailfrom)))
-    # Use this query to test if client_name value is found
+    # and output client_name.
+    client_name_mailfrom = select([Client.client_name]).where(Client.client_email == (str(mailfrom)))
+
+    # Use this query to test if mailfrom value can be found in client_email column of clients table
+    # and output client_id.
+    client_id_mailfrom = select([Client.client_id]).where(Client.client_email == (str(mailfrom)))
+
+    # Use this query to test if client_id value is found in sites table
     # client_name_sites = select([Site.client_id])
 
-    results = engine.execute(client_email_mailfrom)
-    for client in results:
-        print(''.join(map(str,client)))
+    results_name = engine.execute(client_name_mailfrom)
+    results_id = engine.execute(client_id_mailfrom)
 
-    if len(client) > 0:
+    for client_id in results_id:
+        print(''.join(map(str, client_id)))
+
+    for client_name in results_name:
+        print(''.join(client_name))
+
+    if len(client_id) > 0:
         print("Found.")
 
     else:
