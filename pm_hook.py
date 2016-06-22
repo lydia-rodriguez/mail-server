4e8ba9fe042d232432cdc168b1fe548d46c9c056
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2016-06-22 16:56:47 -0500

    adding sites table, 2.4

--- pm_hook.py
+++ pm_hook.py
@@ -12,7 +12,7 @@ def process_message_hook(self, peer, mailfrom, rcpttos, data, engine):
 
     # Use this query to test if mailfrom value can be found in client_email column of clients table
     # and output client_name.
-    client_name_mailfrom = select([Client.client_name]).where(Client.client_email == (str(mailfrom)))
+    # client_name_mailfrom = select([Client.client_name]).where(Client.client_email == (str(mailfrom)))
 
     # Use this query to test if mailfrom value can be found in client_email column of clients table
     # and output client_id.
@@ -21,25 +21,25 @@ def process_message_hook(self, peer, mailfrom, rcpttos, data, engine):
     # Use this query to test if client_id value is found in sites table and output site_id.
     client_sites = select([Site.client_id]).where(Site.client_id) == (str(client_id))
 
-    results_client_name = engine.execute(client_name_mailfrom)
+    # results_client_name = engine.execute(client_name_mailfrom)
     # results_client_id = engine.execute(client_id_mailfrom)
     results_site_id = engine.execute(client_sites)
 
     # for client_id in results_client_id:
     #     print(''.join(map(str, client_id)))
 
-    for client_name in results_client_name:
-        print(''.join(client_name))
+    # for client_name in results_client_name:
+    #     print(''.join(client_name))
 
     for site_id in results_site_id:
         print(''.join(map(str, site_id)))
 
-    if len(client_id) > 0:
-        print("Client Found: " + client_name + client_id)
-        if len(site_id) > 0:
-            print("Site Found: " + site_id)
-        else:
-            print("Site not found.")
+    # if len(client_id) > 0:
+    #     print("Client Found: " + client_name + client_id)
+    if len(site_id) > 0:
+        print("Site Found: " + site_id)
     else:
-        print("Client not found.")
+        print("Site not found.")
+    # else:
+    #     print("Client not found.")
 
