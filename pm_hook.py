5a4028d2d0abc39736aa882b1b33c97d8043370a
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2016-06-22 14:02:50 -0500

    adding sites table, 1.3

--- pm_hook.py
+++ pm_hook.py
@@ -16,7 +16,7 @@ def process_message_hook(self, peer, mailfrom, rcpttos, data, engine):
 
     results = engine.execute(client_email_mailfrom)
     for client in results:
-        print(''.join(client))
+        print(client)
 
     if len(client) > 0:
         print("Found.")
