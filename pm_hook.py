26b41400bcb523bf72a1a61a04d2c928958d2580
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2016-06-22 16:53:13 -0500

    adding sites table, 2.3

--- pm_hook.py
+++ pm_hook.py
@@ -16,17 +16,17 @@ def process_message_hook(self, peer, mailfrom, rcpttos, data, engine):
 
     # Use this query to test if mailfrom value can be found in client_email column of clients table
     # and output client_id.
-    client_id_mailfrom = select([Client.client_id]).where(Client.client_email == (str(mailfrom)))
+    # client_id_mailfrom = select([Client.client_id]).where(Client.client_email == (str(mailfrom)))
 
     # Use this query to test if client_id value is found in sites table and output site_id.
     # client_name_sites = select([Site.client_id]).where(Site.client_id) == (str(client_id))
 
     results_client_name = engine.execute(client_name_mailfrom)
-    results_client_id = engine.execute(client_id_mailfrom)
+    # results_client_id = engine.execute(client_id_mailfrom)
     # results_site_id = engine.execute(client_name_sites)
 
-    for client_id in results_client_id:
-        print(''.join(map(str, client_id)))
+    # for client_id in results_client_id:
+    #     print(''.join(map(str, client_id)))
 
     for client_name in results_client_name:
         print(''.join(client_name))
