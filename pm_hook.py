3e84e9ed755ead58f49b8bc9ca4327433182cb68
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2016-06-22 10:56:58 -0500

    modifying query to add more functionality

--- pm_hook.py
+++ pm_hook.py
@@ -3,15 +3,23 @@ from sqlalchemy import select
 from mail_orm import Client
 
 
-def process_message_hook(mailfrom, engine):
+def process_message_hook(self, peer, mailfrom, rcpttos, data, engine):
     print("\nMessage processed.")
 
-    s = select([Client.client_name]).where(Client.client_email == (str(mailfrom)))
-    results = engine.execute(s)
+    client = ''
 
-#    print(results.fetchall())
+    # Use this query to test if mailfrom value can be found in client_email column of clients table
+    client_email_mailfrom = select([Client.client_name]).where(Client.client_email == (str(mailfrom)))
+
+    # Use this query to test if client_name value is found
+    # client_name_sites = select([])
+    
+    results = engine.execute(client_email_mailfrom)
+    for r in results:
+        print(r)
 
     if len(results.fetchall()) > 0:
         print("Found.")
+
     else:
         print("Not found.")
