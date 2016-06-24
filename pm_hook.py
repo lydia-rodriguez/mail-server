d411a3d6351b28e29c74af26f3bc44604a220b24
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2016-06-24 12:27:19 -0500

    adding sites table, 3.9

--- pm_hook.py
+++ pm_hook.py
@@ -21,28 +21,29 @@ def process_message_hook(self, peer, mailfrom, rcpttos, data, engine):
     # Use this query to test if client_id value is found in sites table and output site_id.
     client_sites = select([Site.client_id]).where(Site.client_id) == (str(client_id))
 
-    try:
-        results_client_name = engine.execute(client_name_mailfrom)
-        results_client_id = engine.execute(client_id_mailfrom)
-        results_site_id = engine.execute(client_sites)
-    finally:
-        engine.close()
+    # results_client_name = engine.execute(client_name_mailfrom)
+    # results_client_id = engine.execute(client_id_mailfrom)
+    # results_site_id = engine.execute(client_sites)
+    #
+    # for client_id in results_client_id:
+    #     print(''.join(map(str, client_id)))
+    #
+    # for client_name in results_client_name:
+    #     print(''.join(client_name))
+    #
+    # for site_id in results_site_id:
+    #     print(''.join(map(str, site_id)))
+    #
+    # if len(client_name) > 0:
+    #     print("Client Found: " + str(client_name) + str(client_id))
+    #     # if len(site_id) > 0:
+    #     #     print("Site Found: " + site_id)
+    #     # else:
+    #     #     print("Site not found.")
+    # else:
+    #     print("Client not found.")
 
-    for client_id in results_client_id:
-        print(''.join(map(str, client_id)))
+    s = engine.executeall(client_name_mailfrom, client_id_mailfrom, client_sites)
 
-    for client_name in results_client_name:
-        print(''.join(client_name))
-
-    for site_id in results_site_id:
-        print(''.join(map(str, site_id)))
-
-    if len(client_name) > 0:
-        print("Client Found: " + str(client_name) + str(client_id))
-        # if len(site_id) > 0:
-        #     print("Site Found: " + site_id)
-        # else:
-        #     print("Site not found.")
-    else:
-        print("Client not found.")
-
+    for c in s:
+        print(c)
