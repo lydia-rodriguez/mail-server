214c86cda8b0dd1e8f825e17ac002f6670d3bcc5
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2016-06-29 12:21:26 -0500

    resolving not finding client or id

--- pm_hook.py
+++ pm_hook.py
@@ -15,32 +15,36 @@ def process_message_hook(self, peer, mailfrom, rcpttos, data, engine):
     # Use this query to test if mailfrom value can be found in client_email column of clients table
     # and output client_name.
     client_name_mailfrom = select([Client.client_name]).where(Client.client_email == (str(mailfrom)))
-
-    with engine.connect() as conn:
-        results_client_name = conn.execute(client_name_mailfrom)
-        ## some code
-        b = results_client_name.fetchall()
-        c = b[0]
-        client_name_dict = dict(zip(c.keys(), c.values()))
-        for key, value in client_name_dict.items():
-            k = key
-            client_name = str(value.encode('utf-8'))
-            # print(k, client_name)
+    try:
+        with engine.connect() as conn:
+            results_client_name = conn.execute(client_name_mailfrom)
+            ## some code
+            b = results_client_name.fetchall()
+            c = b[0]
+            client_name_dict = dict(zip(c.keys(), c.values()))
+            for key, value in client_name_dict.items():
+                k = key
+                client_name = str(value.encode('utf-8'))
+                # print(k, client_name)
+    except:
+        print("Client name not found.")
 
     # Use this query to test if mailfrom value can be found in client_email column of clients table
     # and output client_id.
     client_id_mailfrom = select([Client.client_id]).where(Client.client_email == (str(mailfrom)))
-
-    with engine.connect() as conn:
-        results_client_id = conn.execute(client_id_mailfrom)
-        ## some code
-        b = results_client_id.fetchall()
-        c = b[0]
-        client_id_dict = dict(zip(c.keys(), c.values()))
-        for key, value in client_id_dict.items():
-            k = key
-            client_id = int(value)
-            # print(key, client_id)
+    try:
+        with engine.connect() as conn:
+            results_client_id = conn.execute(client_id_mailfrom)
+            ## some code
+            b = results_client_id.fetchall()
+            c = b[0]
+            client_id_dict = dict(zip(c.keys(), c.values()))
+            for key, value in client_id_dict.items():
+                k = key
+                client_id = int(value)
+                # print(key, client_id)
+    except:
+        print("Client ID not found.")
 
     if len(client_name) > 0:
         print("Client Found: " + str(client_name) + ' ' + str(client_id))
