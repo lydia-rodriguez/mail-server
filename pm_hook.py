ea654a80b5a140dc39f3e08d61a50668828d517c
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2016-06-29 16:22:54 -0500

    debugging

--- pm_hook.py
+++ pm_hook.py
@@ -25,76 +25,26 @@ def process_message_hook(self, peer, mailfrom, rcpttos, data, engine):
             print(type(results_client_id))
             print(results_client_id)
 
+            if not results_client_id:
+                client_names = select([Client.client_id, Client.client_name])
+                client_names_list = conn.execute(client_names).fetchall()
+                for client in client_names_list:
+                    if mailfrom_lwr.count(client[1]) > 0:
+                        client_id = client[0]
+                        print("a string")
+                        print(type(client_id))
+                        print(client_id)
+
+
             site_name_fromClient = select([Site.site_name]).where(Site.client_id == results_client_id[0])
             results_site_name = conn.execute(site_name_fromClient).first()
             print(type(results_site_name))
             print(results_site_name)
 
+
+
     except IndexError:
         print("Client ID not found using mailfrom.")
-    #
-    #     client_name_select = select([Client.client_name])
-    #
-    #     try:
-    #         with engine.connect() as conn:
-    #             results_client_nm = conn.execute(client_nm).fetchall()
-    #             ## some code
-    #             for client in results_client_nm:
-    #                 client = (str(client[0]).lower())
-    #                 if mailfrom_lwr.count(client) > 0:
-    #                     client_name = str(client.encode('utf-8'))
-    #                     break
-    #                 else:
-    #                     client_syn1 = select([Client.synonym1])
-    #                     try:
-    #                         with engine.connect() as conn_syn1:
-    #                             results_client_syn1 = conn_syn1.execute(client_syn1).fetchall()
-    #                             ## some code
-    #                             for syn1 in results_client_syn1:
-    #                                 syn1 = str(syn1[0]).lower()
-    #                                 if mailfrom_lwr.count(syn1) > 0:
-    #                                     print("syn found")
-    #                                     client_syn_name = select([Client.client_name]).where(Client.synonym1 == syn1)
-    #                                     with engine.connect() as conn_clientName:
-    #                                         results_client_name
-    #                                     b = client_syn_name.fetchall()
-    #                                     c = b[0]
-    #                                     client_name_dict = dict(zip(c.keys(), c.values()))
-    #                                     for key, value in client_name_dict.items():
-    #                                         client_name = str(value.encode('utf-8'))
-    #
-    #                                     client_name = str(client_syn_name.encode('utf-8'))
-    #                                     break
-    #                     except IndexError:
-    #                         # client_syn1 = select([Client.synonym1])
-    #                         # try:
-    #                         print("fail")
-    #     except IndexError:
-    #         print("FAIL")
-    #
-    # if len(client_name) > 0:
-    #     print("Client Found: " + str(client_name))
-    #
-    #     # Use this query to test if client_id value is found in sites table and output site_name.
-    #
-    #     try:
-    #         client_sites = select([Site.site_name]).where(Site.client_id == int(client_id))
-    #         with engine.connect() as conn:
-    #             results_site_id = conn.execute(client_sites)
-    #             ## some code
-    #             b = results_site_id.fetchall()
-    #             c = b[0]
-    #             site_name_dict = dict(zip(c.keys(), c.values()))
-    #             for key, value in site_name_dict.items():
-    #                 k = key
-    #                 site_name = str(value.encode('utf-8'))
-    #                 # print(key, site_name)
-    #         if len(site_name) > 0:
-    #             print("Site Found: " + str(site_name))
-    #         else:
-    #             print("Site not found.")
-    #     except:
-    #         print("Site not found.")
 
     else:
         print("Client not found.")
