60ddd34db1abbfcbb544ae8bc63c24ab61efde4c
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2016-06-29 17:07:09 -0500

    debugging

--- pm_hook.py
+++ pm_hook.py
@@ -29,40 +29,26 @@ def process_message_hook(self, peer, mailfrom, rcpttos, data, engine):
                 client_names = select([Client])
                 client_names_list = conn.execute(client_names).fetchall()
                 for client_name in client_names_list:
-                    print(1)
                     if mailfrom_lwr.count(client_name.client_name.lower()) > 0:
-                        print(2)
                         client_id = client_name.client_id
                         break
                 else:
                     for client_syn1 in client_names_list:
-                        print(3)
-                        print(client_syn1)
                         if client_syn1.synonym1:
                             if mailfrom_lwr.count(client_syn1.synonym1.lower()) > 0:
                                 client_id = client_syn1.client_id
-                                print("a string")
-                                print(client_syn1.synonym1)
                                 break
                     else:
                         for client_syn2 in client_names_list:
-                            print(4)
-                            print(client_syn2)
                             if client_syn2.synonym2:
                                 if mailfrom_lwr.count(client_syn2.synonym2.lower()) > 0:
-                                    print("b string")
-                                    print(client_syn2)
                                     client_id = client_syn2.client_id
                                     break
                         else:
                             for client_syn3 in client_names_list:
-                                print(5)
-                                print(client_syn3)
                                 if client_syn3.synonym3:
                                     if mailfrom_lwr.count(client_syn3.synonym3.lower()) > 0:
                                         client_id = client_syn3.client_id
-                                        print("c string")
-                                        print(client_syn3)
                                         break
 
     except Exception as error:
