51f999162c9b6b0600951fff73659ee918dfc499
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2016-06-29 11:38:41 -0500

    debugging

--- pm_hook.py
+++ pm_hook.py
@@ -8,7 +8,7 @@ def process_message_hook(self, peer, mailfrom, rcpttos, data, engine):
 
     connection = engine.connect()
 
-    client_id = '5'
+    client_id = ''
     client_name = ''
     site_name = ''
 
@@ -32,15 +32,16 @@ def process_message_hook(self, peer, mailfrom, rcpttos, data, engine):
     for client_id in results_client_id:
         print("client id:")
         print(''.join(map(str, client_id)))
-        client_id_encoded = client_id.encode('utf-8')
-        print(client_id_encoded)
+        type(client_id)
+        # client_id_encoded = client_id.encode('utf-8')
+        # print(client_id_encoded)
 
     for client_name in results_client_name:
         print("client name:")
         print(''.join(client_name))
 
     # Use this query to test if client_id value is found in sites table and output site_name.
-    client_sites = select([Site.site_name]).where(Site.client_id == int(client_id_encoded))
+    client_sites = select([Site.site_name]).where(Site.client_id == int(client_id))
 
     with engine.connect() as conn:
         results_site_id = conn.execute(client_sites)
@@ -51,7 +52,7 @@ def process_message_hook(self, peer, mailfrom, rcpttos, data, engine):
         print(''.join(site_name))
 
     if len(client_name) > 0:
-        print("Client Found: " + str(client_name) + ' ' + str(client_id_encoded))
+        print("Client Found: " + str(client_name) + ' ' + str(client_id))
         if len(site_name) > 0:
             print("Site Found: " + str(site_name))
         else:
