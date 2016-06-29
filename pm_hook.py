84297838e3ff66432725753a3f6bfb6b7fb7b888
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2016-06-29 17:12:27 -0500

    debugging

--- pm_hook.py
+++ pm_hook.py
@@ -17,43 +17,51 @@ def process_message_hook(self, peer, mailfrom, rcpttos, data, engine):
 
     # Use this query to test if mailfrom value can be found in client_email column of clients table
     # and output client_id.
-    client_id_mailfrom = select([Client.client_id]).where(Client.client_email == mailfrom_lwr)
+    client_id_mailfrom = select([Client])
 
     try:
         with engine.connect() as conn:
-            results_client_id = conn.execute(client_id_mailfrom).first()
+            client_query = conn.execute(client_id_mailfrom).first()
 
-            if results_client_id:
-                client_id = results_client_id[0]
+            for client_email_query in client_query:
+                if client_email_query.client_email:
+                    if mailfrom_lwr.count(client_email_query.client_email.lower()) > 0:
+                        client_id = client_email_query.client_email
             else:
-                client_names = select([Client])
-                client_names_list = conn.execute(client_names).fetchall()
-                for client_name in client_names_list:
+                for client_name in client_query:
                     if mailfrom_lwr.count(client_name.client_name.lower()) > 0:
                         client_id = client_name.client_id
                         break
                 else:
-                    for client_syn1 in client_names_list:
+                    for client_syn1 in client_query:
                         if client_syn1.synonym1:
                             if mailfrom_lwr.count(client_syn1.synonym1.lower()) > 0:
                                 client_id = client_syn1.client_id
                                 break
                     else:
-                        for client_syn2 in client_names_list:
+                        for client_syn2 in client_query:
                             if client_syn2.synonym2:
                                 if mailfrom_lwr.count(client_syn2.synonym2.lower()) > 0:
                                     client_id = client_syn2.client_id
                                     break
                         else:
-                            for client_syn3 in client_names_list:
+                            for client_syn3 in client_query:
                                 if client_syn3.synonym3:
                                     if mailfrom_lwr.count(client_syn3.synonym3.lower()) > 0:
                                         client_id = client_syn3.client_id
                                         break
 
+            if client_id:
+
+                print("Client ID: " + str(client_id))
+
+
+
+
+            else:
+
+                print("Client not found.")
+
     except Exception as error:
         print(error)
-    if client_id:
-        print("Client ID: " + str(client_id))
-    else:
-        print("Client not found.")
+
