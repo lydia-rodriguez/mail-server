50a86b20321023ac2b4dac5444a148dca2634ede
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2016-06-29 11:14:57 -0500

    debugging

--- pm_hook.py
+++ pm_hook.py
@@ -22,8 +22,6 @@ def process_message_hook(self, peer, mailfrom, rcpttos, data, engine):
     # and output client_id.
     client_id_mailfrom = select([Client.client_id]).where(Client.client_email == (str(mailfrom)))
 
-    site_name = 5
-
     with engine.connect() as conn:
         results_client_name = conn.execute(client_name_mailfrom)
         ## some code
