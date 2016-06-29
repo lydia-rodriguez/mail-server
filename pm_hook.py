f4587cd307fe3d5c0f68abe0e329196a784db3f0
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2016-06-29 12:17:51 -0500

    resolving not finding site

--- pm_hook.py
+++ pm_hook.py
@@ -12,8 +12,6 @@ def process_message_hook(self, peer, mailfrom, rcpttos, data, engine):
     client_name = ''
     site_name = ''
 
-
-
     # Use this query to test if mailfrom value can be found in client_email column of clients table
     # and output client_name.
     client_name_mailfrom = select([Client.client_name]).where(Client.client_email == (str(mailfrom)))
@@ -44,22 +42,23 @@ def process_message_hook(self, peer, mailfrom, rcpttos, data, engine):
             client_id = int(value)
             # print(key, client_id)
 
-    # Use this query to test if client_id value is found in sites table and output site_name.
-    client_sites = select([Site.site_name]).where(Site.client_id == int(client_id))
-
-    with engine.connect() as conn:
-        results_site_id = conn.execute(client_sites)
-        ## some code
-        b = results_site_id.fetchall()
-        c = b[0]
-        site_name_dict = dict(zip(c.keys(), c.values()))
-        for key, value in site_name_dict.items():
-            k = key
-            site_name = str(value.encode('utf-8'))
-            # print(key, site_name)
-
     if len(client_name) > 0:
         print("Client Found: " + str(client_name) + ' ' + str(client_id))
+
+        # Use this query to test if client_id value is found in sites table and output site_name.
+        client_sites = select([Site.site_name]).where(Site.client_id == int(client_id))
+
+        with engine.connect() as conn:
+            results_site_id = conn.execute(client_sites)
+            ## some code
+            b = results_site_id.fetchall()
+            c = b[0]
+            site_name_dict = dict(zip(c.keys(), c.values()))
+            for key, value in site_name_dict.items():
+                k = key
+                site_name = str(value.encode('utf-8'))
+                # print(key, site_name)
+
         if len(site_name) > 0:
             print("Site Found: " + str(site_name))
         else:
