4bef75d0e040c11719942373e18f33b0f9710ff2
Lydia Rodriguez <lydia.rodriguez@fsgi.com>
2016-06-29 14:51:20 -0500

    debugging

--- pm_hook.py
+++ pm_hook.py
@@ -56,6 +56,7 @@ def process_message_hook(self, peer, mailfrom, rcpttos, data, engine):
                 results_client_nm = conn.execute(client_nm).fetchall()
                 ## some code
                 for client in results_client_nm:
+                    print(str(client).lower())
                     if mailfrom_str.count(str(client).lower()) > 0:
                         client_name = str(client.encode('utf-8'))
                         print("Client found: " + client_name)
